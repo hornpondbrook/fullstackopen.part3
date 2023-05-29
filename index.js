09c92c788a7041470d4d0bf16c33aa12347db2d3
hornpondbrook <hornpondbrook@gmail.com>
2023-05-28 23:03:53 -0400

    part3 phonebook 3.19 - 3.21
    add validation in schema for name and phone number

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ app.delete('/api/persons/:id', (request, response) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name) {
@@ -171,9 +171,12 @@ app.post('/api/persons', (request, response) => {
         number: body.number
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
+
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -184,7 +187,13 @@ app.put('/api/persons/:id', (request, response, next) => {
         number: body.number
     }
 
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(
+        request.params.id, 
+        person, { 
+            new: true,
+            runValidators: true,
+            context: 'query'            
+        })
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -203,6 +212,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
